4afe1398a83341617d62e002ed641e6b81c83080
Md Mahruf Alam <baisakh2015@gmail.com>
2026-06-03 03:57:49 +0600

    structure update game ref

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'orbit_guard_game.dart';
+import 'overlays/control_overlay.dart';
+import 'overlays/game_hud.dart';
+import 'overlays/game_over_menu.dart';
+import 'overlays/main_menu.dart';
+import 'overlays/pause_menu.dart';
+
 void main() {
-  runApp(const MainApp());
+  runApp(const OrbitGuardApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class OrbitGuardApp extends StatelessWidget {
+  const OrbitGuardApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final game = OrbitGuardGame();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: GameWidget<OrbitGuardGame>(
+          game: game,
+          overlayBuilderMap: {
+            'MainMenu': (context, game) => MainMenu(game: game),
+            'GameHud': (context, game) => GameHud(game: game),
+            'Controls': (context, game) => ControlOverlay(game: game),
+            'PauseMenu': (context, game) => PauseMenu(game: game),
+            'GameOverMenu': (context, game) => GameOverMenu(game: game),
+          },
+          initialActiveOverlays: const ['MainMenu'],
         ),
       ),
     );
