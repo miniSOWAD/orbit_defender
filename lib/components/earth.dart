2e54a1890b802f9660fe54da7a76840cf74303c9
Md Mahruf Alam <baisakh2015@gmail.com>
2026-06-03 07:12:24 +0600

    3rd major update

--- lib/components/earth.dart
+++ lib/components/earth.dart
@@ -6,6 +6,8 @@ import '../orbit_guard_game.dart';
 
 class EarthComponent extends SpriteComponent
     with HasGameRef<OrbitGuardGame>, CollisionCallbacks {
+  String _currentImage = '';
+
   EarthComponent()
       : super(
           size: Vector2.all(GameConfig.earthSize),
@@ -14,12 +16,12 @@ class EarthComponent extends SpriteComponent
 
   @override
   Future<void> onLoad() async {
-    sprite = await gameRef.loadSprite('earth100.png');
     position = gameRef.centerPoint;
+    await updateEarthSprite(force: true);
 
     add(
       CircleHitbox.relative(
-        0.92,
+        0.88,
         parentSize: size,
         anchor: Anchor.center,
       ),
@@ -29,6 +31,31 @@ class EarthComponent extends SpriteComponent
   @override
   void update(double dt) {
     position = gameRef.centerPoint;
+    updateEarthSprite();
     super.update(dt);
+  }
+
+  Future<void> updateEarthSprite({bool force = false}) async {
+    final hp = gameRef.earthHp;
+    String image;
+
+    if (hp <= 0) {
+      image = 'earth0.png';
+    } else if (hp <= 25) {
+      image = 'earth25.png';
+    } else if (hp <= 50) {
+      image = 'earth50.png';
+    } else if (hp <= 100) {
+      image = 'earth100.png';
+    } else if (hp <= 150) {
+      image = 'earth150.png';
+    } else {
+      image = 'earth200.png';
+    }
+
+    if (!force && image == _currentImage) return;
+
+    _currentImage = image;
+    sprite = await gameRef.loadSprite(image);
   }
 }
