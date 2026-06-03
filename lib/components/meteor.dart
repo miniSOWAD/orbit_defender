7daa3801168aee0a4cf899b3095291d8160a958d
Md Mahruf Alam <baisakh2015@gmail.com>
2026-06-03 08:00:19 +0600

    major update 4

--- lib/components/meteor.dart
+++ lib/components/meteor.dart
@@ -3,7 +3,6 @@ import 'package:flame/components.dart';
 
 import '../constants.dart';
 import '../orbit_guard_game.dart';
-import 'earth.dart';
 import 'rocket.dart';
 
 class MeteorComponent extends SpriteComponent
@@ -12,6 +11,7 @@ class MeteorComponent extends SpriteComponent
   final Vector2 startPosition;
 
   late double hp;
+  bool hasHitSomething = false;
 
   MeteorComponent({
     required this.config,
@@ -29,7 +29,7 @@ class MeteorComponent extends SpriteComponent
 
     add(
       CircleHitbox.relative(
-        0.82,
+        0.78,
         parentSize: size,
         anchor: Anchor.center,
       ),
@@ -40,12 +40,45 @@ class MeteorComponent extends SpriteComponent
   void update(double dt) {
     if (gameRef.state != GameState.playing) return;
 
+    _checkSurfaceHitWithEarth();
+    _checkHitWithShip();
+
+    if (hasHitSomething) return;
+
     final direction = (gameRef.centerPoint - position).normalized();
     position += direction * config.speed * dt;
 
     angle += 1.3 * dt;
 
     super.update(dt);
+  }
+
+  void _checkSurfaceHitWithEarth() {
+    final earthRadius = GameConfig.earthSize * 0.44;
+    final meteorRadius = GameConfig.meteorSize * 0.34;
+
+    final surfaceDistance = earthRadius + meteorRadius;
+    final currentDistance = position.distanceTo(gameRef.centerPoint);
+
+    if (currentDistance <= surfaceDistance) {
+      hasHitSomething = true;
+      gameRef.damageEarth(config.earthDamage);
+      removeFromParent();
+    }
+  }
+
+  void _checkHitWithShip() {
+    final shipRadius = GameConfig.shipSize * 0.38;
+    final meteorRadius = GameConfig.meteorSize * 0.34;
+
+    final hitDistance = shipRadius + meteorRadius;
+    final currentDistance = position.distanceTo(gameRef.ship.position);
+
+    if (currentDistance <= hitDistance) {
+      hasHitSomething = true;
+      gameRef.damageShip(config.earthDamage);
+      removeFromParent();
+    }
   }
 
   void takeDamage(double damage) {
@@ -63,11 +96,6 @@ class MeteorComponent extends SpriteComponent
     PositionComponent other,
   ) {
     super.onCollisionStart(intersectionPoints, other);
-
-    if (other is EarthComponent) {
-      gameRef.damageEarth(config.earthDamage);
-      removeFromParent();
-    }
 
     if (other is RocketComponent) {
       takeDamage(other.config.damage);
